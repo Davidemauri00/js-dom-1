c89f3d7dde2126602db343dc6d5e100181dafa4c
Davidemauri00 <dadi.spyro@gmail.com>
2025-12-18 15:27:18 +0100

    JS: script lampadina

---
+++ script.js
@@ -0,0 +1,13 @@
+const buttonEl = document.getElementById("buttonLamp");
+const lampEl = document.getElementById("lamp");
+
+buttonEl.addEventListener("click", function(){
+    if(lampEl.alt === "lampadina spenta"){
+        lampEl.src = "./img/yellow_lamp.png";
+        lampEl.alt = "lampadina accesa";
+    }
+    else{
+        lampEl.src = "./img/white_lamp.png";
+        lampEl.alt = "lampadina spenta";
+    }
+});
